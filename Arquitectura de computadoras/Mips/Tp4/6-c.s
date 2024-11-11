a800dcffa469c7d308b88c3522ff4a82888b12c2
NahCs <133619234+NahCs@users.noreply.github.com>
2024-11-11 18:03:40 -0300

    Update 6-c.s

--- Arquitectura de computadoras/Mips/Tp4/6-c.s
+++ Arquitectura de computadoras/Mips/Tp4/6-c.s
@@ -1,6 +1,7 @@
 c) Calcular la suma de los 3 valores utilizando un lazo, con una dirección base de 0 y un registro como puntero.
 Pista: Cargar la dirección en un registro con daddi $t2, $zero, V y luego cargar los valores con ld $t1,
 0($t2)
+## En caso de descargar el archivo borrar enunciado, sino el mips no te lo toma ##
 
 .data
 V: .word 16, 16, 16
